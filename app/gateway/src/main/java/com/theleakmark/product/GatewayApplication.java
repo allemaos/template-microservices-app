e804e709cb454a3cb878894906ae58c8e2717891
Giorgos Andreou <allemaos@gmail.com>
2017-10-03 15:44:01 +0100

    clean up

--- app/gateway/src/main/java/com/theleakmark/product/GatewayApplication.java
+++ app/gateway/src/main/java/com/theleakmark/product/GatewayApplication.java
@@ -2,11 +2,13 @@ package com.theleakmark.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import com.theleakmark.product.filters.pre.SimpleFilter;
 
 @EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
 
