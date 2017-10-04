676840d7b8daa99a445208e757afe30559369b8b
Giorgos Andreou <allemaos@gmail.com>
2017-10-04 11:27:58 +0100

    reset ip's to resolve ip issues

--- test_microservices_ips.sh
+++ test_microservices_ips.sh
@@ -2,23 +2,23 @@
 
 
 echo '==========Client ================'
-curl http://localhost:7071/health
+curl http://localhost:8070/health
 
 echo '====== Edge Server (gateway) ====='
 curl http://localhost:7070/health
 
 echo '====== Hystrix Dashboard ========='
-curl http://localhost:7061/health
+curl http://localhost:7002/health
 
 echo '========= Turbine ==============='
-curl http://localhost:7062/health
+curl http://localhost:7003/health
 
 echo '========= Product ==============='
-curl http://localhost:9090/health
+curl http://localhost:7072/health
 
 echo '======= Discovery Service ========'
 curl http://localhost:8761/health
 
 echo '========== Book ================'
-curl http://localhost:8090/health
+curl http://localhost:7073/health
 
