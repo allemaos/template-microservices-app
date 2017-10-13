517ea4947ecf3f72a0af114eb82089a8194c6ad0
Giorgos Andreou <allemaos@gmail.com>
2017-10-14 02:09:37 +0300

    new config file for turbine-stream microservice

--- app/turbine/src/main/java/com/theleakmark/turbine/TurbineApplication.java
+++ app/turbine/src/main/java/com/theleakmark/turbine/TurbineApplication.java
@@ -4,14 +4,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.StandardEnvironment;
 
 /**
  * @author Spencer Gibb
  * @author Dave Syer
  */
-@Configuration
 @EnableAutoConfiguration
 @EnableTurbineStream
 @EnableDiscoveryClient
@@ -19,7 +17,7 @@ public class TurbineApplication {
 
 	public static void main(String[] args) {
 		boolean cloudEnvironment = new StandardEnvironment().acceptsProfiles("cloud");
-		//new SpringApplicationBuilder(TurbineApplication.class).web(!cloudEnvironment).run(args);
-		new SpringApplicationBuilder(TurbineApplication.class).run(args);
+		new SpringApplicationBuilder(TurbineApplication.class).web(!cloudEnvironment).run(args);
+//		new SpringApplicationBuilder(TurbineApplication.class).run(args);
 	}
 }
